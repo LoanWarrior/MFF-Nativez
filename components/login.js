7aa0c46e9acddb08d95ae1d539e090602d7338fb
Peter S <petersenn4296@gmail.com>
2018-07-24 15:06:23 -0600

    form is set up to log someone in, capturing data

--- components/login.js
+++ components/login.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, Image, Button, TouchableOpacity} from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
+import t from 'tcomb-form-native'; // 0.6.9
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  username: t.String,
+  password: t.String,
+  Owner: t.Boolean
+});
+
 
 export default class Login extends Component {
+
+  handleSubmit = () => {
+    const value = this._form.getValue()
+  }
 
   render() {
     const { navigate } = this.props.navigation
     return (
       <View style={styles.container}>
-        <Text>Form to sign in or sign up here</Text>
-        <Button
-          onPress={() => {navigate('LoggedIn')}}
-          title="Go to the logged in page"
-          color="#841584"
-        />
+        <Text style={styles.header}>Log in!!!</Text>
+          <Form type={User} ref={c => this._form = c} />
+          <Button
+            onPress={this.handleSubmit}
+            title="Log in"
+            color="#841584"
+          />
+          <Button
+            onPress={() => {navigate('LoggedIn')}}
+            title="Sign up"
+            color="#841584"
+          />
       </View>
     );
   }
@@ -25,5 +47,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'lightblue',
+  },
+  header: {
+    fontSize: 50
+  },
+  inputs: {
+    width: 100
   }
 })
