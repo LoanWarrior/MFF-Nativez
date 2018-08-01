81545cdde5c5a418eb6046326d9cd7a065c5b92d
Jeff S <Slater.Jeff7@gmail.com>
2018-08-01 13:39:39 -0600

    all working!!!! 1.40

--- components/specific-truck.js
+++ components/specific-truck.js
@@ -27,7 +27,6 @@ class SpecificTruck extends Component {
     const { navigate } = this.props.navigation
     const orders = this.props.orders
     let orderInfo = []
-    console.log(orders);
     if (orders) {
       for ( let order in orders){
         let items = ''
