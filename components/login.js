144d69676273c7675c9e90609e6f5d2b33238aff
Aiden Whelton <aidenwhelton@yahoo.com>
2018-08-02 10:13:38 -0600

    final styling

--- components/login.js
+++ components/login.js
@@ -40,9 +40,9 @@ class Login extends Component {
           barStyle= "light-content"
           hidden = {false}
         />
-
-        {/* <Image source={require('../images/food-truck.jpg')}/> */}
-        <Text>{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}</Text>
+        <Text>{"\n"}{"\n"}</Text>
+        <Image source={require('../images/absKermit.png')}/>
+        <Text>{"\n"}</Text>
         <Text style={styles.header}>Log in</Text>
         <Text>{"\n"}</Text>
           <Form type={User} ref={c => this._form = c}/>
@@ -53,7 +53,7 @@ class Login extends Component {
               color="#1A3647"
             />
           </View>
-          <Text>{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}{"\n"}</Text>
+          <Text>{"\n"}</Text>
           <Text style={styles.anyText}>Don't have an account?{"\n"}</Text>
           <View style={styles.buttonContainer}>
             <Button
